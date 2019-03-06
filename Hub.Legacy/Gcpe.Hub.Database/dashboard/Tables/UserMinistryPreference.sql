9ad1ea5705c74c5d31198ec4566a70f582bb6000
Andre Lashley <andre.lashley@gmail.com>
2019-03-06 11:48:56 -0800

    Add a table for UserMinistryPreference

---
+++ Hub.Legacy/Gcpe.Hub.Database/dashboard/Tables/UserMinistryPreference.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dashboard].[UserMinistryPreference]
+(
+	[Email] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [MinistryId] UNIQUEIDENTIFIER NULL, 
+	CONSTRAINT [FK_UserMinistryPreference_Ministry] FOREIGN KEY ([MinistryId]) REFERENCES [dbo].[Ministry] ([Id])
+)
