972a77c1f3b4c60ebd782f1461be3d66db2d197e
Andre Lashley <andre.lashley@gmail.com>
2019-03-11 10:59:43 -0700

    Change tabs to spaces in UserMinistryPreference.sql file

--- Hub.Legacy/Gcpe.Hub.Database/dashboard/Tables/UserMinistryPreference.sql
+++ Hub.Legacy/Gcpe.Hub.Database/dashboard/Tables/UserMinistryPreference.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dashboard].[UserMinistryPreference]
 (
-	[Email] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Email] NVARCHAR(50) NOT NULL PRIMARY KEY, 
     [MinistryId] UNIQUEIDENTIFIER NULL, 
-	CONSTRAINT [FK_UserMinistryPreference_Ministry] FOREIGN KEY ([MinistryId]) REFERENCES [dbo].[Ministry] ([Id])
+    CONSTRAINT [FK_UserMinistryPreference_Ministry] FOREIGN KEY ([MinistryId]) REFERENCES [dbo].[Ministry] ([Id])
 )
