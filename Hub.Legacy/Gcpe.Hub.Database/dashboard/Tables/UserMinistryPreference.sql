a124cc906aace739d575b7ec2be7a84324dcea3b
André Lashley <andre.lashley@gmail.com>
2019-03-12 16:02:01 -0700

    Add MinistryId as the second part of ministry user preference composite key

--- Hub.Legacy/Gcpe.Hub.Database/dashboard/Tables/UserMinistryPreference.sql
+++ Hub.Legacy/Gcpe.Hub.Database/dashboard/Tables/UserMinistryPreference.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dashboard].[UserMinistryPreference]
 (
     [Email] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [MinistryId] UNIQUEIDENTIFIER NULL, 
+    [MinistryId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
     CONSTRAINT [FK_UserMinistryPreference_Ministry] FOREIGN KEY ([MinistryId]) REFERENCES [dbo].[Ministry] ([Id])
 )
