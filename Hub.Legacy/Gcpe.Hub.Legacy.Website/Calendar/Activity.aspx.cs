0c1f96b41b8420a9d4b671f7dbfb69110104ddb5
Carmichael <Alexander.Carmichael@gov.bc.ca>
2023-04-05 09:45:49 -0700

    Revert "Revert "Make significance field not mandatory (#108)""
    This reverts commit bb05079ad287a854b2a1ac7311b0ad51294e9670.

--- Hub.Legacy/Gcpe.Hub.Legacy.Website/Calendar/Activity.aspx.cs
+++ Hub.Legacy/Gcpe.Hub.Legacy.Website/Calendar/Activity.aspx.cs
@@ -225,7 +225,6 @@ namespace Gcpe.Hub.Calendar
             {
                 //HQ users see all fields
                 DetailsRequiredFieldValidator.Visible = false;
-                SignificanceRequiredFieldValidator.Visible = false;
                 SchedulingFieldValidator.Visible = false;
             }
             else
@@ -236,14 +235,6 @@ namespace Gcpe.Hub.Calendar
                     LACommentsRow.Visible = false;
                     LAStatusRow.Visible = false;
                     laFieldset.Style.Add("display", "none");
-                }
-
-                if (string.IsNullOrEmpty(CurrentActiveActivity.Significance) && !Settings.Default.ShowSignificanceField)
-                {
-                    SignificanceRow.Style.Add("display", "none");
-                    // if we are not showing this field, do not validate - cannot enter data, cannot pass validation.
-                    SignificanceRequiredFieldValidator.Visible = false;
-                    SignificanceRequiredFieldValidator.Enabled = false;
                 }
 
                 if (string.IsNullOrEmpty(CurrentActiveActivity.Schedule) &&
