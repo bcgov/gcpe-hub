9dbead1c373c861d22a0782a20d6bb3e842c8d61
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2023-04-19 15:35:25 -0700

    not necessary changes

--- Hub.Legacy/Gcpe.Hub.Legacy.Website/Properties/Settings.Designer.cs
+++ Hub.Legacy/Gcpe.Hub.Legacy.Website/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Gcpe.Hub.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool SignificanceIsRequired {
             get {
                 return ((bool)(this["SignificanceIsRequired"]));
